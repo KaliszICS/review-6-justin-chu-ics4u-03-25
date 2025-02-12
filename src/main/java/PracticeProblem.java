0071738f5e1e98de4e97e50fa65d08a211a2b3db
justin-chu-ics4u-03-25 <justin.chu4@student.tdsb.on.ca>
2025-02-12 05:35:56 +0000

    my answers

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,57 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static int sum(int a, int b) {
+
+		return a + b;
+
+	}
+
+
+	public static int difference(int a, int b) {
+
+		return a - b;
+
+	}
+
+	public static double product(double a, double b) {
+
+		return a * b;
+
+	}
+
+	public static String removeFirst (String sussyballs) {
+
+		return sussyballs.substring(1);
+
+	}
+
+	public static int combinedLength(String amongusone, String amongustwo) {
+
+		return amongusone.length() + amongustwo.length();
+	}
+
+	public static boolean isEven(int n) {
+
+		return n % 2 == 0;
+
+	}
+
+	public static boolean isOdd(int n) {
+
+		return n % 2 != 0;
+
+	}
+
+
+	public static boolean isPositive (int n) {
+
+		return n > 0;
+
+	}
+
+	public static boolean isNegative (int n) {
+
+		return n < 0;
 
 	}
 
